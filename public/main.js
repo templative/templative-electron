a80437f0e73fc98a655b2eeec78d4224da2f7c65
Oliver Barnum <oliverbarnum32@gmail.com>
2023-11-24 14:24:21 -0600

    Render larger

--- public/main.js
+++ public/main.js
@@ -3,15 +3,15 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-      width: 800,
-      height: 600,
+      width: 1920,
+      height: 1080,
       webPreferences: {
         nodeIntegration: true
       }
     })
   
     win.loadURL('http://localhost:3000');
-    // win.webContents.openDevTools()
+    win.webContents.openDevTools()
   }
 
 app.whenReady().then(() => {
