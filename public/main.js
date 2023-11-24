4caf36779454843854119c03e57d34684c7ccdf9
Oliver Barnum <oliverbarnum32@gmail.com>
2023-11-24 14:04:48 -0600

    Add react app

--- public/main.js
+++ public/main.js
@@ -4,10 +4,13 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      }
     })
   
-    win.loadFile('index.html')
+    win.loadURL('http://localhost:3000');
     // win.webContents.openDevTools()
   }
 
