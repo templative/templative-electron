87bbee5253700de43f85a783b42d208061cc62fa
Oliver Barnum <oliverbarnum32@gmail.com>
2023-11-25 17:57:15 -0600

    Render components

--- src/components/TemplativeProject.js
+++ src/components/TemplativeProject.js
@@ -2,14 +2,20 @@ const fs = window.require('fs');
 const path = window.require("path");
 
 export default class TemplativeProject {
-    static readFile(templativeRootDirectoryPath, fileName) {            
-        return JSON.parse(fs.readFileSync(path.join(templativeRootDirectoryPath, fileName), 'utf8'));
+    componentCompose = undefined
+    gameCompose = undefined
+    gameFile = undefined
+    studioFile = undefined
+
+    static readFile(templativeRootDirectoryPath, fileName) {
+        var filepath = path.join(templativeRootDirectoryPath, fileName);            
+        return JSON.parse(fs.readFileSync(filepath, 'utf8'));
     }
+
     constructor(templativeRootDirectoryPath) {
-        var componentCompose = TemplativeProject.readFile(templativeRootDirectoryPath, "component-compose.json");
-        var gameCompose = TemplativeProject.readFile(templativeRootDirectoryPath, "game-compose.json");
-        var gameFile = TemplativeProject.readFile(templativeRootDirectoryPath, "game.json");
-        var studioFile = TemplativeProject.readFile(templativeRootDirectoryPath, "studio.json");
-        console.log(componentCompose, gameCompose, gameFile, studioFile)
+        this.componentCompose = TemplativeProject.readFile(templativeRootDirectoryPath, "component-compose.json");
+        this.gameCompose = TemplativeProject.readFile(templativeRootDirectoryPath, "game-compose.json");
+        this.gameFile = TemplativeProject.readFile(templativeRootDirectoryPath, "game.json");
+        this.studioFile = TemplativeProject.readFile(templativeRootDirectoryPath, "studio.json");
     }
 }
--- src/components/TemplativeProjectRenderer.jsx
+++ src/components/TemplativeProjectRenderer.jsx
@@ -15,8 +15,24 @@ export default class TemplativeProjectRenderer extends React.Component {
     componentWillUnmount() {
         ipcRenderer.removeAllListeners(channels.GIVE_TEMPLATIVE_ROOT_FOLDER);
     }
+
+    renderComponentDivs() {
+        if (this.state.templativeProject === undefined) {
+            return [];
+        }
+        var divs = [];
+        console.log(this.state.templativeProject)
+        for(var i = 0; i < this.state.templativeProject.componentCompose.length; i++) {
+            var component = this.state.templativeProject.componentCompose[i]
+            divs.push(<p key={component.name}>{component.name}</p>)
+        }
+        return divs;
+    }
+
     render() {
+        var componentDivs = this.renderComponentDivs()
         return <div>
+            {componentDivs}
         </div>        
     }
 }
