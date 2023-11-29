33adfc51fd2af8ff63063a5835d6b06795e31312
Oliver Barnum <oliverbarnum32@gmail.com>
2023-11-29 04:13:38 -0600

    Actually kill pytohn server

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ import sys
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import templative
+import os, signal
 
 app = Flask(__name__)
 app_config = {"host": "0.0.0.0", "port": sys.argv[1]}
 
-"""
----------------------- DEVELOPER MODE CONFIG -----------------------
-"""
 # Developer mode uses app.py
 if "app.py" in sys.argv[0]:
   # Update app config
@@ -24,29 +22,23 @@ if "app.py" in sys.argv[0]:
   app.config["CORS_HEADERS"] = "Content-Type"
 
 
-"""
---------------------------- REST CALLS -----------------------------
-"""
 # Remove and replace with your own
-@app.route("/example")
-async def example():
-  # print(templative.cli())
+@app.route("/render", methods = ['POST']) 
+async def render():
   await templative.produce.gameProducer.produceGame("C:/Users/User/Documents/git/nextdaygames/apcw-defines", None, False, False, "en")
   # See /src/components/App.js for frontend call
   return jsonify("Example response from Flask! Learn more in /app.py & /src/components/App.js")
 
 
-"""
--------------------------- APP SERVICES ----------------------------
-"""
 # Quits Flask on Electron exit
 @app.route("/quit")
 def quit():
-  shutdown = request.environ.get("werkzeug.server.shutdown")
-  shutdown()
-
-  return
-
+  # shutdown = request.environ.get("werkzeug.server.shutdown")
+  # if shutdown is None:
+  #   raise RuntimeError('Not running with the Werkzeug Server')
+  # shutdown()
+  os.kill(os.getpid(), signal.SIGINT)
+  return "Killed server"
 
 if __name__ == "__main__":
   app.run(**app_config)
