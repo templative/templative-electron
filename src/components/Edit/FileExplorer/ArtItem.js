4003f07397f07211ed69c7563480e46c5d1b316a
Oliver Barnum <oliverbarnum32@gmail.com>
2023-11-30 10:50:59 -0600

    Launch inkscape

--- src/components/Edit/FileExplorer/ArtItem.js
+++ src/components/Edit/FileExplorer/ArtItem.js
@@ -1,14 +1,32 @@
 import React from "react";
 import "./Art.css"
 const path = window.require("path");
+const shell = window.require('electron').shell;
+var debounceDurationMilliseconds = 3000
 
 export default class ArtItem extends React.Component {   
-    state = {isHovering: false}
+    state = {
+        isHovering: false,
+        lastLaunchTime: this.getCurrentTime()
+    }
     handleMouseOver = () => {
         this.setState({isHovering: true})
     }
     handleMouseOut = () => {
         this.setState({isHovering: false})
+    }
+    getCurrentTime() {
+        return new Date().getTime()
+    }
+    openFile = () => {
+        console.log(this.getCurrentTime() - this.state.lastLaunchTime)
+        if (this.getCurrentTime() - this.state.lastLaunchTime < debounceDurationMilliseconds) {
+            return
+        }
+        shell.openPath(this.props.filepath);
+        this.setState({
+            lastLaunchTime: this.getCurrentTime()
+        })
     }
     render() {
         var callback = () => this.props.updateViewedFileCallback("ART", this.props.filepath)
@@ -17,7 +35,10 @@ export default class ArtItem extends React.Component {
             onMouseLeave={this.handleMouseOut}
         >
             <p className="artItem" onClick={callback}>
-                {path.parse(this.props.filename).name} {this.state.isHovering && <button className="btn btn-dark goto">🡭</button>}
+                {path.parse(this.props.filename).name} 
+                {this.state.isHovering &&
+                    <button onClick={()=> this.openFile()}className="btn btn-dark goto">🡭</button>
+                }
             </p>
         </div>
     }
