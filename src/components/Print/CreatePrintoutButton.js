e11b6f6dd65c2621600c16aed3f2b2c87bf2505a
Oliver Barnum <oliverbarnum32@gmail.com>
2024-01-05 03:36:36 -0600

    Fix indents

--- src/components/Print/CreatePrintoutButton.js
+++ src/components/Print/CreatePrintoutButton.js
@@ -13,20 +13,20 @@ export default class CreatePrintoutButton extends React.Component {
         }
         return <div className='create-printout-button-container'>
             <div className="input-group input-group-sm printout-button-controls" data-bs-theme="dark">
-                    <select value={this.props.size} onChange={(e)=>{this.props.setSizeCallback(e.target.value)}} className="form-select" id="inputGroupSelect01">
-                        <option value="Letter">Letter</option>
-                        <option value="Tabloid">Tabloid</option>
-                    </select>
-                    <span className="input-group-text">Include Back?</span>
-                    <div className="input-group-text">
-                        <input className="form-check-input mt-0" type="checkbox" value="" onChange={()=>{ this.props.toggleIsBackIncludedCallback()}} checked={this.props.isBackIncluded} aria-label="Checkbox for following text input" />
-                    </div>
-                    <span className="input-group-text">Draw Margins?</span>
-                    <div className="input-group-text">
-                        <input className="form-check-input mt-0" type="checkbox" value="" onChange={()=>{ this.props.toggleAreMarginsDrawnCallback()}} checked={this.props.areMarginsDrawn} aria-label="Checkbox for following text input"/>
-                    </div>
+                <select value={this.props.size} onChange={(e)=>{this.props.setSizeCallback(e.target.value)}} className="form-select" id="inputGroupSelect01">
+                    <option value="Letter">Letter</option>
+                    <option value="Tabloid">Tabloid</option>
+                </select>
+                <span className="input-group-text">Include Back?</span>
+                <div className="input-group-text">
+                    <input className="form-check-input mt-0" type="checkbox" value="" onChange={()=>{ this.props.toggleIsBackIncludedCallback()}} checked={this.props.isBackIncluded} aria-label="Checkbox for following text input" />
                 </div>
-                <button disabled={this.props.isCreatingPrintout || !this.props.hasOutputDirectoryValue} type="button" className="btn btn-outline-secondary create-printout-button" onClick={() => this.props.createPrintoutCallback()}>{buttonMessage}</button>            
+                <span className="input-group-text">Draw Margins?</span>
+                <div className="input-group-text">
+                    <input className="form-check-input mt-0" type="checkbox" value="" onChange={()=>{ this.props.toggleAreMarginsDrawnCallback()}} checked={this.props.areMarginsDrawn} aria-label="Checkbox for following text input"/>
+                </div>
+            </div>
+            <button disabled={this.props.isCreatingPrintout || !this.props.hasOutputDirectoryValue} type="button" className="btn btn-outline-secondary create-printout-button" onClick={() => this.props.createPrintoutCallback()}>{buttonMessage}</button>            
         </div>
     }
 }
