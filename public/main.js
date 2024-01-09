476de28de20a05d8ffc2ca244ceb1630b1ff3f8e
Oliver Barnum <oliverbarnum32@gmail.com>
2024-01-09 05:57:10 -0600

    Use startup window

--- public/main.js
+++ public/main.js
@@ -32,6 +32,27 @@ const createWindow = () => {
         await shutdown()
     })
 }
+const createStartupWindow = () => {
+  var startupWindow = new BrowserWindow({
+    width: 125 * 2, height: 100 * 2,
+    title: "Templative Startup",
+    center:true, resizable:false, movable: true, minimizable: false,
+    maximizable: false, closable: false, focusable: false, alwaysOnTop: false,
+    fullscreenable: false, skipTaskbar: true, frame: false, hiddenInMissionControl: true,
+    titleBarStyle: "hidden", backgroundColor: '#282c34',
+    show: false,
+    icon: __dirname + "/favicon.ico",
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      devTools: true,
+      webSecurity: false,
+    },
+  })
+  startupWindow.loadURL(`file://${__dirname}/startup.html`);
+  startupWindow.show()
+  return startupWindow
+}
 
 const createServerManager = () => {
   var templativeServerCommandsByEnvironment = {
@@ -68,8 +89,7 @@ const launchServers = async () => {
 }
 
 app.whenReady().then(async () => {
-  // installImageMagick()
-  
+  var startupWindow = createStartupWindow()
   var serverStartResult = await launchServers()
   if (serverStartResult == 0) {
     console.log("Failure!")
@@ -77,6 +97,8 @@ app.whenReady().then(async () => {
     return
   }
   createWindow()
+  startupWindow.closable=true
+  startupWindow.close()
   app.on('activate', () => {
       if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
