957675a4b1ba08cf425228eac2781c4393404e50
Oliver Barnum <oliverbarnum32@gmail.com>
2024-01-21 03:29:29 -0600

    Fix warnings

--- src/components/Create/TagFilter.js
+++ src/components/Create/TagFilter.js
@@ -2,7 +2,7 @@ export const componentTypeHasAllFilteredTags = (selectedTags, componentTypeTags)
     for(const tagToFilterBy of selectedTags) {
         var hasTag = false
         for (var i = 0 ; i < componentTypeTags.length ; i++) {
-            if (tagToFilterBy != componentTypeTags[i]) {
+            if (tagToFilterBy !== componentTypeTags[i]) {
                 continue
             }
             hasTag = true
