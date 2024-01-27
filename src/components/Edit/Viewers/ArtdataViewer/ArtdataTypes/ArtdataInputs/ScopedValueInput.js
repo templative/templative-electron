4ec0c64e16edeb387d955558b28c215f480d1d4e
Oliver Barnum <oliverbarnum32@gmail.com>
2024-01-26 19:32:55 -0600

    Add utility function chooser

--- src/components/Edit/Viewers/ArtdataViewer/ArtdataTypes/ArtdataInputs/ScopedValueInput.js
+++ src/components/Edit/Viewers/ArtdataViewer/ArtdataTypes/ArtdataInputs/ScopedValueInput.js
@@ -2,8 +2,23 @@ import React from "react";
 import "../../ArtdataViewer.css"
 
 export default class ScopedValueInput extends React.Component {   
-    
+    createSelectionOption = (scope) => {
+        const defaultInput = <input type="text" className="form-control" onChange={(event)=>this.props.updateArtdataFieldCallback(this.props.index, "source", event.target.value)} aria-label="What key to get from the scope..." value={this.props.source}/>
+        
+        const utilitySelectInput = <select value={this.props.scope} onChange={(event)=>this.props.updateArtdataFieldCallback(this.props.index, "source", event.target.value)} className="form-select">
+            <option value="git-sha">Current Git Commit Hash</option>
+        </select>
+        
+        switch(scope) {
+            case "utility":
+                return utilitySelectInput
+            default: 
+                return defaultInput
+        }
+    } 
+
     render() {
+        const selectionOption = this.createSelectionOption(this.props.scope)
         return <React.Fragment>
                 <span className="input-group-text">❔</span>
                 <select value={this.props.scope} onChange={(event)=>this.props.updateArtdataFieldCallback(this.props.index, "scope", event.target.value)} className="form-select scope-select" id="inputGroupSelect01">
@@ -14,7 +29,7 @@ export default class ScopedValueInput extends React.Component {
                     <option value="piece">Piece</option>
                     <option value="utility">Utility</option>
                 </select>
-                <input type="text" className="form-control" onChange={(event)=>this.props.updateArtdataFieldCallback(this.props.index, "source", event.target.value)} aria-label="What key to get from the scope..." value={this.props.source}/>
+                {selectionOption}
             </React.Fragment>
     }
 }
