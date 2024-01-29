5750cf78f0bc47f4c823a4e5a4d8572090871a70
Oliver Barnum <oliverbarnum32@gmail.com>
2024-01-29 07:16:08 -0600

    Copy incomplete advertisment stuff

---
+++ src/components/Upload/Advertisements/AdvertisementSelection.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./AdvertismentFiles.css"
+
+export default class AdvertisementSelection extends React.Component {   
+    render() {        
+        return <div className="advertisement-interface">
+            <p>Hello</p>
+        </div>
+    }
+}
