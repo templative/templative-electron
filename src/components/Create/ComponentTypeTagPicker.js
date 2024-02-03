80231a5a59a2b7a5bc75bb8a687d0b01eace2bee
Oliver Barnum <oliverbarnum32@gmail.com>
2024-02-02 23:46:04 -0600

    Cleanup tags

--- src/components/Create/ComponentTypeTagPicker.js
+++ src/components/Create/ComponentTypeTagPicker.js
@@ -12,26 +12,45 @@ export const addComponentTypeTagsToComponentTagOptions = (componentTagOptions, c
         }
     }
 }
+
 export default class ComponentTypeTagPicker extends React.Component {   
     render() {
         var componentTagOptions = {}
-        Object.keys(this.props.componentTypeOptions).forEach((key) => {
-            var componentOption = this.props.componentTypeOptions[key]
-            if (componentOption["Tags"] === undefined) {
-                return
-            }
-            if (!componentTypeHasAllFilteredTags(this.props.selectedTags, componentOption["Tags"])) {
-                return
-            }
-            addComponentTypeTagsToComponentTagOptions(componentTagOptions, componentOption["Tags"])
-        })
-        var sortedTags = Object.keys(componentTagOptions)
-            .filter((tag) => componentTagOptions[tag] >= 1)
-            .sort((a, b) => {
-                if (a < b) {
+        Object.keys(this.props.componentTypeOptions)
+            .filter((key) => {
+                var componentOption = this.props.componentTypeOptions[key]
+                const componentTypeHasNoTags = componentOption["Tags"] === undefined
+                if (componentTypeHasNoTags) {
+                    return false
+                }
+                const componentTypeWouldBeFilteredByCurrentTags = !componentTypeHasAllFilteredTags(this.props.selectedTags, componentOption["Tags"])
+                if (componentTypeWouldBeFilteredByCurrentTags) {
+                    return false
+                }
+                return true
+            })
+            .forEach((key) => {
+
+                var componentOption = this.props.componentTypeOptions[key]
+                addComponentTypeTagsToComponentTagOptions(componentTagOptions, componentOption["Tags"])
+            })
+        var sortedTagElements = Object.keys(componentTagOptions)
+            .filter((tag) => {
+                const tagWouldHasMoreThanOneItemInIt = componentTagOptions[tag] >= 1
+                const tagIsBlank = tag.trim() === ""
+                return tagWouldHasMoreThanOneItemInIt && !tagIsBlank
+            })
+            .sort((tagA, tagB) => {
+                // if (componentTagOptions[tagA] < componentTagOptions[tagB]) {
+                //     return 1;
+                // }
+                // if (componentTagOptions[tagA] > componentTagOptions[tagB]) {
+                //     return -1;
+                // }
+                if (tagA < tagB) {
                     return -1;
                 }
-                if (a > b) {
+                if (tagA > tagB) {
                     return 1;
                 }
                 return 0;
@@ -39,14 +58,14 @@ export default class ComponentTypeTagPicker extends React.Component {
             .map((tag) => {
                 return <button 
                     key={tag} 
-                    onClick={()=> this.props.toggleTagFilterCallback(tag)} 
-                    className={`btn tag-button ${this.props.selectedTags.has(tag) && "selected-tag"}`}>
-                        {tag}
+                    onClick={()=> this.props.toggleTagFilterCallback(tag.trim())} 
+                    className={`btn tag-button ${this.props.selectedTags.has(tag.trim()) && "selected-tag"}`}>
+                        {tag.trim()}
                 </button>
             })
 
         return <div className="component-tag-options">
-            {sortedTags}
+            {sortedTagElements}
         </div>
     }
 }
