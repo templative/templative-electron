09841b89a199e8ef4213d99a329daef602152660
Oliver Barnum <oliverbarnum32@gmail.com>
2024-04-22 17:58:17 -0500

    Try using imports

--- python/templative/__main__.py
+++ python/templative/__main__.py
@@ -1,4 +1,13 @@
 from templative import cli
+import asyncclick
+import anyio
+import aiofile
+import aiohttp
+import tabulate
+import fpdf
+import markdown2
+import svgmanip
+import numpy
 
 if __name__ == '__main__':
     cli()
