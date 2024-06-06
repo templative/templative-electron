c8c610750f1f203f04ac224ff11e296d80647f8a
Oliver Barnum <oliverbarnum32@gmail.com>
2024-06-06 17:52:56 -0500

    Encrypt my ass

--- src/app/sessionStore.js
+++ src/app/sessionStore.js
@@ -4,44 +4,45 @@ const storage = require('node-persist');
 const crypto = require('crypto');
 
 // Initialize storage
-console.log(path.join(app.getPath('userData'), 'secure-storage'))
-storage.init({
-    dir: path.join(app.getPath('userData'), 'secure-storage'),
-    stringify: JSON.stringify,
-    parse: JSON.parse,
-    encoding: 'utf8',
-    logging: false,
-    continuous: true,
-    interval: false,
-    ttl: false
-});
+(async () => {
+    await storage.init({
+        dir: path.join(app.getPath('userData'), 'secure-storage'),
+        stringify: JSON.stringify,
+        parse: JSON.parse,
+        encoding: 'utf8',
+        logging: false,
+        continuous: true,
+        interval: false,
+        ttl: false
+    });
+})();
 
 const staticBytes = [
     0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
     0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10,
     0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18,
     0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20
-  ];
-  
+];
+
 const ENCRYPTION_KEY = Buffer.from(staticBytes);
 const IV_LENGTH = 16; 
 
 function encrypt(text) {
     let iv = crypto.randomBytes(IV_LENGTH);
-    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
-    let encrypted = cipher.update(text);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return iv.toString('hex') + ':' + encrypted.toString('hex');
+    let cipher = crypto.createCipheriv('aes-256-cbc', ENCRYPTION_KEY, iv);
+    let encrypted = cipher.update(text, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    return iv.toString('hex') + ':' + encrypted;
 }
 
 function decrypt(text) {
     let textParts = text.split(':');
     let iv = Buffer.from(textParts.shift(), 'hex');
     let encryptedText = Buffer.from(textParts.join(':'), 'hex');
-    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
-    let decrypted = decipher.update(encryptedText);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-    return decrypted.toString();
+    let decipher = crypto.createDecipheriv('aes-256-cbc', ENCRYPTION_KEY, iv);
+    let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
 }
 
 const getSessionToken = async () => {
