d5fa9ce911a4a21d7004455c17f2157f239bc6e1
Oliver Barnum <oliverbarnum32@gmail.com>
2024-06-07 04:21:35 -0500

    Print component directory

--- python/templative/cli/server/__init__.py
+++ python/templative/cli/server/__init__.py
@@ -3,12 +3,13 @@ import aiohttp_cors
 import asyncclick as click
 from .httpRoutes import routes
 from .socketRoutes import sio
+import sys
 
 @click.command()
 @click.option('--port', default=8080, required=False, help='The port of the local server.')
 async def serve(port):
     """Serve Templative as a socket and http server"""
-    
+    print("Serving template files from %s" % sys._MEIPASS)
     app = web.Application()
     app.add_routes(routes)
     sio.attach(app)
