a8ba45c6afb1c8a21d1c278c9c8459f9a3c916c1
Oliver Barnum <oliverbarnum32@gmail.com>
2024-07-10 10:48:00 -0500

    Speed up code signing

--- signWindowsInS3.sh
+++ signWindowsInS3.sh
@@ -11,16 +11,21 @@ fi
 
 download_files() {
     local version=$1
+    echo "Downloading s3://$TEMP_BUCKET/Templative-$version\ Setup.exe $LOCAL_DIR/Templative-$version\ Setup.exe"
     aws s3 cp s3://$TEMP_BUCKET/Templative-$version\ Setup.exe $LOCAL_DIR/Templative-$version\ Setup.exe
+
+    echo "s3://$TEMP_BUCKET/Templative-win32-x64-$version.zip $LOCAL_DIR/Templative-win32-x64-$version.zip"
     aws s3 cp s3://$TEMP_BUCKET/Templative-win32-x64-$version.zip $LOCAL_DIR/Templative-win32-x64-$version.zip
 }
 unzip_file() {
     local version="$1"
+    echo "Unzipping $LOCAL_DIR/Templative-win32-x64-$version.zip"
     unzip "$LOCAL_DIR/Templative-win32-x64-$version.zip" -d "$LOCAL_DIR/Templative-win32-x64-$version"
     rm "$LOCAL_DIR/Templative-win32-x64-$version.zip"
 }
 rezip_directory() {
     local version="$1"
+    echo "Rezipping"
     (cd "$LOCAL_DIR" && zip -r "Templative-win32-x64-$version.zip" "Templative-win32-x64-$version")
     rm -rf $LOCAL_DIR/Templative-win32-x64-$version
 }
@@ -29,13 +34,14 @@ sign_setup_exe() {
     local version=$1
     TIMESTAMP_SERVER="http://timestamp.sectigo.com"
 
-    echo "Signing..."
+    echo "Signing Setup.exe"
     signtool sign -sha1 "$GONEXTGAMES_CODESIGNINGCERTIFICATE_THUMBPRINT" -tr "$TIMESTAMP_SERVER" -td sha256 -fd sha256 "$LOCAL_DIR/Templative-$version Setup.exe"
     signtool sign -sha1 "$GONEXTGAMES_CODESIGNINGCERTIFICATE_THUMBPRINT" -tr "$TIMESTAMP_SERVER" -td sha256 -fd sha256 "$LOCAL_DIR/Templative-win32-x64-$version/templative-$version Setup.exe"
 }
 
 upload_signed_files() {
     local version=$1
+    echo "Uploading Setup, zip, and nupkg's"
     aws s3 cp $LOCAL_DIR/Templative-$version\ Setup.exe s3://$FINAL_BUCKET/Templative-$version\ Setup.exe
     aws s3 cp "$LOCAL_DIR/Templative-win32-x64-$version.zip" "s3://$FINAL_BUCKET/Templative-win32-x64-$version.zip"
     aws s3 cp "s3://$TEMP_BUCKET/templative-$version-delta.nupkg" s3://$FINAL_BUCKET/templative-$version-delta.nupkg
@@ -43,6 +49,7 @@ upload_signed_files() {
 
 }
 remove_local_files() {
+    echo "Removing local setup and zip"
     rm "$LOCAL_DIR/Templative-$version Setup.exe"
     rm "$LOCAL_DIR/Templative-win32-x64-$version.zip"
 }
@@ -58,7 +65,7 @@ process_setup_file() {
     # The only reason we create this the zip file is so that new downloads dont wait as the zip of the nupkg are assembled.
     if aws s3api head-object --bucket "templative-artifacts" --key "win32/x64/Templative-win32-x64-$version.zip" >/dev/null 2>&1; then
         echo "templative-artifacts/win32/x64/Templative-$version Setup.exe already exists so we are skipping."
-        return 1
+        return 2
     fi
 
     download_files "$version"
@@ -100,10 +107,13 @@ copy_releases_file() {
 check_for_new_files() {
     echo "Checking for new setup.exe in $TEMP_BUCKET..."
 
-    setup_files=$(aws s3 ls "s3://$TEMP_BUCKET/" | grep 'Setup.exe' | awk '{print $4}')
+    setup_files=$(aws s3 ls "s3://$TEMP_BUCKET/" | grep 'Setup.exe' | awk '{print $4}' | tac)
 
     for setup_file in $setup_files; do
         process_setup_file "$setup_file"
+        if [ $? -eq 2 ]; then
+            break
+        fi
     done
     copy_releases_file
 }
