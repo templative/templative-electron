e7c8d4b5d256a05b116e1ea9b2fd7e29203294a5
Oliver Barnum <oliverbarnum32@gmail.com>
2024-07-10 05:15:26 -0500

    Rotate logs and make sure dir exists

--- src/app/logger.js
+++ src/app/logger.js
@@ -1,13 +1,24 @@
 const log = require('electron-log');
-const os = require('os')
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
 
-console.log(`Writing logs to ${os.homedir() + "/Documents/Templative/log.log"}`)
+const logDir = path.join(os.homedir(), 'Documents', 'Templative');
+const logFilePath = path.join(logDir, 'log.log');
+
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir, { recursive: true });
+}
+
+console.log(`Writing logs to ${logFilePath}`);
 
 log.transports.file.level = 'info';
-log.transports.file.resolvePathFn = () => os.homedir() + "/Documents/Templative/log.log";
+log.transports.file.resolvePathFn = () => logFilePath;
+
+log.transports.file.maxSize = 5 * 1024 * 1024;
 
 module.exports = {
     log: (entry) => log.info(entry),
     error: (entry) => log.error(entry),
     warn: (entry) => log.warn(entry)
-}
+};
