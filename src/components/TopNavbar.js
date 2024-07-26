e27dc4b9917bd9cc59094ab929b5e9d242f77467
Oliver Barnum <oliverbarnum32@gmail.com>
2024-07-26 04:33:57 -0500

    Add topbar explanations

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -1,26 +1,32 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import "./TopNavbar.css"
+import "./TopNavbar.css";
 import { Link } from 'react-router-dom';
 
-class TopNavbar extends React.Component {   
+class TopNavbar extends React.Component {
     render() {
         var elements = this.props.topNavbarItems.map((topNavbarItem) => {
-            var isSelectedRoute = this.props.currentRoute === topNavbarItem.route
-            var buttonClasses = "btn btn-secondary " + (isSelectedRoute ? "currentRoute" : "")
-            return <li key={topNavbarItem.name} className="nav-item">
-                <Link onClick={()=> {this.props.updateRouteCallback(topNavbarItem.route)}} to={topNavbarItem.route} className={"nav-link"}>
-                    <button className={buttonClasses}>
-                        {topNavbarItem.svg}
-                    </button>
-                </Link>
-            </li>
-        })
-        return <div className="topNavbar">
-            <ul className="nav flex-column justify-content-center sidebar-buttons">
-                {elements}
-            </ul>
-        </div>
+            var isSelectedRoute = this.props.currentRoute === topNavbarItem.route;
+            var buttonClasses = "btn btn-secondary " + (isSelectedRoute ? "currentRoute" : "");
+            return (
+                <li key={topNavbarItem.name} className="nav-item">
+                    <Link onClick={() => { this.props.updateRouteCallback(topNavbarItem.route) }} to={topNavbarItem.route} className={"nav-link"}>
+                        <button className={buttonClasses}>
+                            {topNavbarItem.svg}
+                        </button>
+                        <div className="tooltip">{topNavbarItem.name}</div>
+                    </Link>
+                </li>
+            );
+        });
+        return (
+            <div className="topNavbar">
+                <ul className="nav flex-column justify-content-center sidebar-buttons">
+                    {elements}
+                </ul>
+            </div>
+        );
     }
 }
+
 export default TopNavbar;
