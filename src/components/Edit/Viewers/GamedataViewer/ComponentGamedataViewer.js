9ff56a7a707eb43e626070351365ef145dfc3abb
Oliver Barnum <oliverbarnum32@gmail.com>
2024-10-02 22:52:30 -0500

    Switch emoji plus to text on viewer files

--- src/components/Edit/Viewers/GamedataViewer/ComponentGamedataViewer.js
+++ src/components/Edit/Viewers/GamedataViewer/ComponentGamedataViewer.js
@@ -123,7 +123,11 @@ export default class ComponentGamedataViewer extends EditableViewerJson {
                     </div>
                     {rows}
                     <div className="input-group input-group-sm mb-3" data-bs-theme="dark">
-                        <button onClick={() => this.addBlankKeyValuePair()} className="btn btn-outline-secondary add-button" type="button" id="button-addon1">➕</button>
+                        <button onClick={() => this.addBlankKeyValuePair()} className="btn btn-outline-secondary add-button" type="button" id="button-addon1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-lg add-field-plus" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+                            </svg>
+                            Add Field to Component
+                        </button>
                     </div>
                 </div>
             </div>
--- src/components/Edit/Viewers/GamedataViewer/PieceGamedataViewer.js
+++ src/components/Edit/Viewers/GamedataViewer/PieceGamedataViewer.js
@@ -151,7 +151,12 @@ export default class PieceGamedataViewer extends EditableViewerJson {
             <div className="col">
                 <div className="row add-pieces-gamedata-row">
                     <div key="addPieceButton" className="input-group input-group-sm mb-3 add-piece-button" data-bs-theme="dark">
-                        <button onClick={() => this.addPiece()} className="btn btn-outline-secondary add-button" type="button" id="button-addon1">Create a new Piece</button>
+                        <button onClick={() => this.addPiece()} className="btn btn-outline-secondary add-button" type="button" id="button-addon1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-lg add-field-plus" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+                            </svg>
+                            Create a new Piece
+                        </button>
                     </div>
                 </div>
                 <div className="row pieces-gamedata-row">
