4d8a3d1fdbee621d99b284c7803292418dbee1e2
Oliver Barnum <oliverbarnum32@gmail.com>
2024-11-22 20:15:19 -0600

    Add rotation to images

--- src/components/Edit/Viewers/ImageViewer.js
+++ src/components/Edit/Viewers/ImageViewer.js
@@ -9,6 +9,7 @@ export default class ImageViewer extends React.Component {
         this.state = {
             remountKey: 0, // A key to trigger re-renders
             timestamp: Date.now(), // Used to bust the browser cache
+            rotation: 0, // Add rotation state
         };
         this.fileWatcher = null;
     }
@@ -20,7 +21,8 @@ export default class ImageViewer extends React.Component {
 
     componentDidUpdate(prevProps) {
         if (prevProps.filepath !== this.props.filepath) {
-            // If the file path changes, restart the file watcher
+            // If the file path changes, restart the file watcher and reset rotation
+            this.setState({ rotation: 0 });
             this.startFileWatcher();
         }
     }
@@ -56,15 +58,60 @@ export default class ImageViewer extends React.Component {
         }
     }
 
+    rotateImage = () => {
+        this.setState(prevState => ({
+            rotation: (prevState.rotation + 90) % 360
+        }));
+    }
+
     render() {
         const { filepath } = this.props;
-        const { timestamp } = this.state;
+        const { timestamp, rotation } = this.state;
+
+        // Calculate if we need to swap dimensions (at 90° or 270°)
+        const isVertical = rotation % 180 !== 0;
+        
+        // Get rotation class based on current rotation
+        const getRotationClass = () => {
+            const normalizedRotation = rotation % 360;
+            switch(normalizedRotation) {
+                case 90: return 'rotate-90';
+                case 180: return 'rotate-180';
+                case 270: return 'rotate-270';
+                default: return '';
+            }
+        };
 
         return (
             <div className="row image-viewer" key={this.state.remountKey}>
-                <div className="col" align="center">
-                    {/* Add timestamp to force reload */}
-                    <img className="stretchedImage" src={`file://${filepath}?t=${timestamp}`} alt=""/>
+                <div className="col image-viewer-col" align="center">
+                    <div className="image-viewer-controls">
+                        <button className="rotate-button" onClick={this.rotateImage}>
+                            ⟲
+                        </button>
+                    </div>
+                    <div className="image-viewer-container">
+                        {isVertical ? (
+                            <div className="rotation-wrapper-outer">
+                                <div className="rotation-wrapper-inner">
+                                    <img 
+                                        className={`stretched-image vertical-rotation ${getRotationClass()}`}
+                                        src={`file://${filepath}?t=${timestamp}`} 
+                                        alt=""
+                                        draggable="false"
+                                    />
+                                </div>
+                            </div>
+                        ) : (
+                            <img 
+                                className="stretched-image"
+                                src={`file://${filepath}?t=${timestamp}`} 
+                                alt=""
+                                style={{ transform: `rotate(${rotation}deg)` }}
+                                draggable="false"
+                            />
+                        )}
+                    </div>
                 </div>
             </div>
         );
