83f7c625a209517fd4ae328dd38c9826286cc93c
Oliver Barnum <oliverbarnum32@gmail.com>
2025-01-30 06:11:35 -0600

    Fix output sizing

--- src/renderer/app/components/Render/RenderedOutput/OutputExplorer.js
+++ src/renderer/app/components/Render/RenderedOutput/OutputExplorer.js
@@ -70,54 +70,50 @@ export default function OutputExplorer({ outputFolderPath, templativeRootDirecto
     
     return (
         <React.Fragment>
-            <div className="row render-progress-row">
-                <div className="col">
-                    {outputName && (
-                        <>
-                            <div className="output-header-container">
-                                <p className="output-folder-name">{metadata.gameDisplayName}</p>
-                                <p className="output-folder-version-info">- {metadata.versionName}</p>
-                                <p className="output-folder-version-info">v{metadata.versionNumber}</p>
-                                <p className="output-folder-component-filter">{metadata.componentFilter}</p>
-                                {metadata.timestamp && 
-                                    <p className="outputFolderName output-folder-date">{metadata.timestamp}</p>
-                                }
+            <div className="render-progress-row">
+                {outputName && (
+                    <>
+                        <div className="output-header-container">
+                            <p className="output-folder-name">{metadata.gameDisplayName}</p>
+                            <p className="output-folder-version-info">- {metadata.versionName}</p>
+                            <p className="output-folder-version-info">v{metadata.versionNumber}</p>
+                            <p className="output-folder-component-filter">{metadata.componentFilter}</p>
+                            {metadata.timestamp && 
+                                <p className="outputFolderName output-folder-date">{metadata.timestamp}</p>
+                            }
+                        </div>
+                    
+                        {percentageDone < 100 ? (
+                            <div className="progress render-progress-bar-outer">
+                                <div 
+                                    className={`progress-bar render-progress-bar-inner ${
+                                        percentageDone !== 100 && "progress-bar-striped progress-bar-animated"
+                                    }`} 
+                                    role="progressbar" 
+                                    style={style}
+                                    aria-valuenow={percentageDone.toString()} 
+                                    aria-valuemin="0" 
+                                    aria-valuemax="100"
+                                />
                             </div>
-                        
-                            {percentageDone < 100 ? (
-                                <div className="progress render-progress-bar-outer">
-                                    <div 
-                                        className={`progress-bar render-progress-bar-inner ${
-                                            percentageDone !== 100 && "progress-bar-striped progress-bar-animated"
-                                        }`} 
-                                        role="progressbar" 
-                                        style={style}
-                                        aria-valuenow={percentageDone.toString()} 
-                                        aria-valuemin="0" 
-                                        aria-valuemax="100"
-                                    />
-                                </div>
-                            ) : (
-                                <div className="post-render-options">
-                                    {PostRenderOptions.map((option, index) => (
-                                        <p 
-                                            key={`tab-${option}`}
-                                            className={renderingContext.exportOptionIndex === index ? "selected-post-render-option" : ""}
-                                            onClick={() => renderingContext.setExportOptionIndex(index)}
-                                        >
-                                            {option}
-                                        </p>
-                                    ))}
-                                </div>
-                            )}
-                        </>
-                    )}
-                </div>
+                        ) : (
+                            <div className="post-render-options">
+                                {PostRenderOptions.map((option, index) => (
+                                    <p 
+                                        key={`tab-${option}`}
+                                        className={renderingContext.exportOptionIndex === index ? "selected-post-render-option" : ""}
+                                        onClick={() => renderingContext.setExportOptionIndex(index)}
+                                    >
+                                        {option}
+                                    </p>
+                                ))}
+                            </div>
+                        )}
+                    </>
+                )}
             </div>
-            <div className="row render-output-row">
-                <div className="col">
-                    {views[renderingContext.exportOptionIndex]}
-                </div>
+            <div className="render-output-row">
+                {views[renderingContext.exportOptionIndex]}
             </div>
         </React.Fragment>
     );
