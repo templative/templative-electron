ad578c9c10ff3e3555b81851ec3e8f5a9b115bd6
Oliver <oliverbarnum32@gmail.com>
2025-02-27 10:31:44 -0600

    Disable react leaflet

--- src/renderer/app/components/Map/controls/Map.js
+++ src/renderer/app/components/Map/controls/Map.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 // import {publishers, conventions} from "../mapInfo"
 import dayjs from 'dayjs';
